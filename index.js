c909e13cb3574205c72fc9317a935541ac00c947
marvinroger <nivramdu94@gmail.com>
2015-08-09 11:30:36 +0000

    Prevent bug with week day

--- index.js
+++ index.js
@@ -29,14 +29,14 @@ app.get('/', function(req, res) {
 app.post('/timetable', function(req, res) {
   var errors = [];
   if (req.get('Content-Type') != 'application/json') { return res.status(400).json({ errors: ['Content-Type must be application/json'] }); }
-  if (!req.body.hasOwnProperty('projectId') || !utils.isInteger(req.body.projectId)) { errors.push('projectId must exist and be an integer') }
-  if (!req.body.hasOwnProperty('resourcesId') || !utils.isInteger(req.body.resourcesId)) { errors.push('resourcesId must exist and be an integer') }
-  dateRegex = /^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/
-  if (!req.body.hasOwnProperty('startDate') || !dateRegex.test(req.body.startDate)) { errors.push('startDate must exist and be an ISO 8601 date') }
-  else if (new Date(req.body.startDate).getDay() != 1) { errors.push('startDate must be a monday') }
+  if (!req.body.hasOwnProperty('projectId') || !utils.isInteger(req.body.projectId)) { errors.push('projectId must exist and be an integer'); }
+  if (!req.body.hasOwnProperty('resourcesId') || !utils.isInteger(req.body.resourcesId)) { errors.push('resourcesId must exist and be an integer'); }
+  var dateRegex = /^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/;
+  if (!req.body.hasOwnProperty('startDate') || !dateRegex.test(req.body.startDate)) { errors.push('startDate must exist and be an ISO 8601 date'); }
+  else if (new Date(req.body.startDate).getDay() != 1) { errors.push('startDate must be a monday'); }
   if (errors.length > 0) { return res.status(400).json({ errors: errors }); }
   
-  timetable.get(req.body.projectId, req.body.resourcesId, new Date(req.body.startDate), moment(req.body.startDate).day(6).toDate(), function(err, timetable) {
+  timetable.get(req.body.projectId, req.body.resourcesId, new Date(req.body.startDate), moment(req.body.startDate).isoWeekday(6).toDate(), function(err, timetable) {
     if (err) {
       console.log(err);
       return res.status(500).json({errors: ['Error gathering timetable from the UPEC ADE']});
